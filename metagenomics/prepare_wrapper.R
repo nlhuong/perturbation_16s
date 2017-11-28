82170011173e7838c5fd84e694c09610fa8b53d3
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-27 18:25:56 -0800

    more batches

--- metagenomics/prepare_wrapper.R
+++ metagenomics/prepare_wrapper.R
@@ -11,7 +11,7 @@
 input_files <- list.files("../data/", "*.fq", full.names = TRUE)
 input_files <- unique(gsub("_1P.fq||_2P.fq", "", input_files))
 n_files <- length(input_files)
-n_per_batch <- 5
+n_per_batch <- 2
 
 for (i in seq(1, n_files, n_per_batch)) {
   system(sprintf("bash submit.sh %s %s", i, i + n_per_batch - 1))
