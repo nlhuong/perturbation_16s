cbec4fa518b5d43654658f276ac61256a4e65153
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-27 16:47:16 -0800

    wrap across all input files

---
+++ metagenomics/prepare_wrapper.R
@@ -0,0 +1,22 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+##
+## Wrapper for prepare.R, that allows samples to be processed in parallel on
+## sherlock.
+##
+## author: sankaran.kris@gmail.com
+## date: 11/27/2017
+
+input_files <- list.files("../data/", "*.fq", full.names = TRUE)
+input_files <- gsub("_1P.fq||_2P.fq", "", input_files)
+n_files <- length(input_files)
+n_per_batch <- 5
+
+start_ix <- seq(1, n_files, n_per_batch)
+
+for (i in start_ix) {
+  for (j in i:(i + n_per_batch - 1)) {
+    system(sprintf("bash submit.sh %s %s", i, j))
+  }
+}
