3b0aafaa78841db03a831ae84bd60d4a503b35f7
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-27 16:50:18 -0800

    use argparser

--- metagenomics/prepare.R
+++ metagenomics/prepare.R
@@ -8,14 +8,16 @@
 ## author: sankaran.kris@gmail.com
 ## date: 11/27/2017
 
-###############################################################################
-## Some analysis using MIDAS
-###############################################################################
 library("stringr")
-start_ix <- 1
-end_ix <- 5
+library("argparser")
+parser <- arg_parser("Prepare metagenomic data for statistical analysis")
+parser <- add_argument(parser, "--start_ix", help = "Start index of files for input", default = 1)
+parser <- add_argument(parser, "--end_ix", help = "End index of files for input", default = 5)
+argv <- parse_args(parser)
 
-## setup paths
+###############################################################################
+## Setup paths and load modules
+###############################################################################
 midas_path <- "/scratch/users/kriss1/applications/MIDAS"
 python_path <- sprintf("%s:%s", Sys.getenv("PYTHONPATH"), midas_path)
 path <- sprintf("%s:%s", Sys.getenv("PATH"), file.path(midas_path, "scripts"))
@@ -23,16 +25,22 @@ Sys.setenv("PYTHONPATH" = python_path)
 Sys.setenv("PATH" = path)
 Sys.setenv("MIDAS_DB" = file.path(midas_path, "database", "midas_db_v1.2"))
 
-## load required modules
 system("module load biology")
 system("module load samtools/1.6")
 
+###############################################################################
+## Define input and output directories
+###############################################################################
 outdir <- "../data/processed/"
 dir.create("../data/processed/")
 
 input_files <- list.files("../data/", "*.fq", full.names = TRUE)
 input_files <- gsub("_1P.fq||_2P.fq", "", input_files)
 input_files <- input_files[start_ix:end_ix]
+
+###############################################################################
+## Loop over input, performing profiling one file at a time
+###############################################################################
 
 for (f in input_files) {
   f1 <- paste0(f, "_1P.fq")
