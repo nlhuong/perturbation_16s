17f83dc9fdda3436e7d6beb925c935286f607b62
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-27 16:46:36 -0800

    submit batch jobs for midas

---
+++ metagenomics/submit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+sbatch <<EOT
+#!/bin/bash
+#
+#all commands that start with SBATCH contain commands that are just used by SLURM for scheduling
+#################
+#set a job name
+#SBATCH --job-name=midas_profiling
+#################
+#a file for job output, you can check job progress, append the job ID with %j to make it unique
+#SBATCH --output=pipeline%j.out
+#################
+# a file for errors from the job
+#SBATCH --error=pipeline%j.err
+#################
+#time you think you need; default is 2 hours
+#format could be dd-hh:mm:ss, hh:mm:ss, mm:ss, or mm
+#SBATCH --time=01:00:00
+#################
+#Quality of Service (QOS); think of it as job priority, there is also --qos=long for with a max job length of 7 days, qos normal is 48 hours.
+# REMOVE "normal" and set to "long" if you want your job to run longer than 48 hours,
+# NOTE- in the hns partition the default max run time is 7 days , so you wont need to include qos
+
+#SBATCH --qos=normal
+
+# We are submitting to the dev partition, there are several on sherlock: normal, gpu, owners, hns, bigmem (jobs requiring >64Gigs RAM)
+#
+#SBATCH -p normal,hns
+#################
+#number of nodes you are requesting, the more you ask for the longer you wait
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nodes=1
+#################
+
+# Have SLURM send you an email when the job ends or fails, careful, the email could end up in your clutter folder
+# Also, if you submit hundreds of jobs at once you will get hundreds of emails.
+
+#SBATCH --mail-type=END,FAIL # notifications for job done & fail
+# Remember to change this to your email
+#SBATCH --mail-user=kriss1@stanford.edu
+
+#now run normal batch commands
+cd /scratch/users/kriss1//research/perturbation_16/metagenomics
+Rscript prepare.R $1 $2
+EOT
