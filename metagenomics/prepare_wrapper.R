ce381c939b457b7d8e13636b595f7572dadb18e3
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-27 17:36:38 -0800

    Run more files, but let each batch take longer

--- metagenomics/prepare_wrapper.R
+++ metagenomics/prepare_wrapper.R
@@ -10,8 +10,7 @@
 
 input_files <- list.files("../data/", "*.fq", full.names = TRUE)
 input_files <- gsub("_1P.fq||_2P.fq", "", input_files)
-## n_files <- length(input_files)
-n_files <- 15
+n_files <- length(input_files)
 n_per_batch <- 5
 
 for (i in seq(1, n_files, n_per_batch)) {
