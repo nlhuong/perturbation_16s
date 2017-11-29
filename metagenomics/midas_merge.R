489bbad80a6329ae7040860edb144f7c48ae9fa8
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-29 09:32:25 -0800

    write midas genes / snp results to a single matrix

--- metagenomics/midas_merge.R
+++ metagenomics/midas_merge.R
@@ -8,6 +8,15 @@
 ## author: sankaran.kris@gmail.com
 ## date: 11/29/2017
 
+library("tidyverse")
+library("feather")
+
+bind_wrapper <- function(x) {
+  x %>%
+    bind_rows %>%
+    select(species, gene_id, starts_with("M"))
+}
+
 ###############################################################################
 ## Setup PATH to MIDAS
 ###############################################################################
@@ -35,3 +44,31 @@ system(sprintf(base_cmd, "snps", genes_dirs, samples_dir))
 ###############################################################################
 ## Combine gene coverage and depth data
 ###############################################################################
+genes_f <- list.files(genes_dirs, full.names = TRUE)
+depths <- list()
+copy_num <- list()
+snps <- list()
+snps_info <- list()
+
+for (i in seq_along(genes_f)) {
+  message("Merging ", genes_f[i])
+  depths[[i]] <- read_tsv(file.path(genes_f[i], "genes_depth.txt"))
+  depths[[i]]$species <- basename(genes_f[i])
+
+  copy_num[[i]] <- read_tsv(file.path(genes_f[i], "genes_copynum.txt"))
+  copy_num[[i]]$species <- basename(genes_f[i])
+
+  snps[[i]] <- read_tsv(file.path(genes_f[i], "snps_depth.txt"))
+  snps[[i]]$species <- basename(genes_f[i])
+
+  snps_info[[i]] <- read_tsv(file.path(genes_f[i], "snps_info.txt"))
+  snps_info[[i]]$species <- basename(genes_f[i])
+}
+
+###############################################################################
+## Write results to file
+###############################################################################
+write_feather(bind_wrapper(depths), file.path(merged_dir, "depths.txt"))
+write_feather(bind_wrapper(copy_num), file.path(merged_dir, "copy_num.txt"))
+write_feather(bind_wrapper(snps), file.path(merged_dir, "snps.txt"))
+write_feather(bind_wrapper(snps_info), file.path(merged_dir, "snps_info.txt"))
