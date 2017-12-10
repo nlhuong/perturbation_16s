4b19dd1e4c22dcc5445b1e66f0085061697b9b61
Kris Sankaran <sankaran.kris@gmail.com>
2017-12-09 23:30:32 -0800

    update the metagenomics plots, now that we have lots of species

--- metagenomics/descriptive.R
+++ metagenomics/descriptive.R
@@ -12,6 +12,7 @@
 ## Setup and data
 ###############################################################################
 library("tidyverse")
+library("argparser")
 library("readxl")
 library("feather")
 library("pheatmap")
@@ -39,13 +40,14 @@ theme_update(
 
 parser <- arg_parser("Plot MIDAS output")
 parser <- add_argument(parser, "--subdir", help = "The subdirectory of data/ containing all the processed data", default = "metagenomic")
-parser <- add_argument("k_cov", help = "k in k-over-a filter for coverage", default = 0.2)
-parser <- add_argument("a_cov", help = "a in k-over-a filter for coverage", default = 0)
-parser <- add_argument("k_dep", help = "k in k-over-a filter for depths", default = 0.2)
-parser <- add_argument("a_dep", help = "a in k-over-a filter for depths", default = 0)
+parser <- add_argument(parser, "--k_cov", help = "k in k-over-a filter for coverage", default = 0.05)
+parser <- add_argument(parser, "--a_cov", help = "a in k-over-a filter for coverage", default = 0)
+parser <- add_argument(parser, "--k_depth", help = "k in k-over-a filter for depths", default = 0.3)
+parser <- add_argument(parser, "--a_depth", help = "a in k-over-a filter for depths", default = 25)
 argv <- parse_args(parser)
 
 merged_dir <- file.path("..", "data", argv$subdir, "merged")
+coverage <- read_tsv(file.path(merged_dir, "coverage.tsv"))
 depths <- read_feather(file.path(merged_dir, "depths.feather"))
 copy_num <- read_feather(file.path(merged_dir, "copy_num.feather"))
 mapping <- read_xlsx("../data/Mapping_Files_7bDec2017.xlsx", skip = 1)
@@ -53,7 +55,7 @@ mapping <- read_xlsx("../data/Mapping_Files_7bDec2017.xlsx", skip = 1)
 ###############################################################################
 ## Study the species COG coverage data
 ###############################################################################
-keep_ix <- rowMeans(coverage[, -1] > parser$k_cov) > parser$a_cov
+keep_ix <- rowMeans(coverage[, -1] > argv$k_cov) > argv$a_cov
 coverage <- coverage[keep_ix, ]
 coverage[, -1] <- asinh(coverage[, -1])
 
@@ -110,10 +112,28 @@ ggplot(loadings) +
 ###############################################################################
 ## Gene depths
 ###############################################################################
-depths_df <- depths[, -c(1, 2)] %>%
-  as.data.frame()
+keep_ix <- rowMeans(depths[, -c(1, 2)] > argv$a_depth, na.rm = TRUE) > argv$k_depth
+
+depths_df <- depths[keep_ix, ] %>%
+  as.data.frame() %>%
+  separate(
+    species,
+    c("genus", "species_name", "strain_id"), "_",
+    remove = FALSE
+  ) %>%
+  mutate(
+    genus = as.factor(genus),
+    genus_grouped = fct_lump(genus, prop = 0.04)
+  )
+rm(depths)
+depths_df[is.na(depths_df)] <- 0
+
 rownames(depths_df) <- depths$gene_id
-pheatmap(t(depths_df), show_colnames = FALSE)
+pheatmap(
+  t(depths_df %>% select(starts_with("M"))),
+  annotate_rows = depths_df$genus_grouped,
+  show_colnames = FALSE
+)
 
 ## can also make a PCA biplot
 depths_mat <- depths %>%
