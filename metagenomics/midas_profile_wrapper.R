a7c9f6589d321bee1dbe8f75080ba22fe2f883aa
Kris Sankaran <sankaran.kris@gmail.com>
2017-12-11 11:31:21 -0800

    accept path to generic data directory

--- metagenomics/midas_profile_wrapper.R
+++ metagenomics/midas_profile_wrapper.R
@@ -8,11 +8,16 @@
 ## author: sankaran.kris@gmail.com
 ## date: 11/27/2017
 
-input_files <- list.files("../data/metagenomic/", "*.fq", full.names = TRUE)
+library("argparser")
+parser <- arg_parser("Wrap metagenomic and metatranscriptomic profiling")
+parser <- add_argument(parser, "--subdir", help = "The subdirectory of data/ containing all the raw data", default = "metagenomic")
+
+indir <- file.path("..", "data", argv$subdir)
+input_files <- list.files(indir, "*.fq", full.names = TRUE)
 input_files <- unique(gsub("_1P.fq||_2P.fq", "", input_files))
 
 ## Identify and filter away procesed samples
-processed_files <- list.files("../data/metagenomic/processed", full.names = TRUE)
+processed_files <- list.files(file.path(indir, "processed"), full.names = TRUE)
 for (f in processed_files) {
   snps_dir <- file.path(f, "snps", "output")
   if (length(list.files(snps_dir)) > 0) {
@@ -24,7 +29,7 @@ n_files <- length(input_files)
 n_per_batch <- 4
 
 for (i in seq(1, n_files, n_per_batch)) {
-  cmd <- sprintf("bash submit.sh %s %s", i, min(n_files, i + n_per_batch - 1))
+  cmd <- sprintf("bash submit.sh %s %s %s", i, min(n_files, i + n_per_batch - 1), indir)
   message(cmd)
   system(cmd)
 }
