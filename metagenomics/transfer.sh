d480f82ff0f9c1e785bc75c94ea717fae096283f
Kris Sankaran <sankaran.kris@gmail.com>
2017-12-13 10:28:21 -0800

    compress when transferring fqgz's too

--- metagenomics/transfer.sh
+++ metagenomics/transfer.sh
@@ -3,9 +3,9 @@
 ## Transfers raw metagenomic data to data/ directory
 ##
 ## author: sankaran.kris@gmail.com
-## date: 11/27/2017
+## date: 12/13/2017
 
 ## Would be nice if the data are gzipped...
 rsync --compress --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaG/HMD_Mar2017/*Sub/*.fq ../data/metagenomic
-rsync --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Sub/*.fq.gz ../data/metatranscriptomic
-rsync --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Plate*/*.fq.gz ../data/metatranscriptomic
+rsync --compress --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Sub/*.fq.gz ../data/metatranscriptomic
+rsync --compress --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Plate*/*.fq.gz ../data/metatranscriptomic
