50473bc4980d871f2212bb74dbdd1f8fe3e6c3a8
Lan Huong Nguyen <lanhuong@sh-ln03.stanford.edu>
2017-12-21 05:35:47 -0800

    Change matagenomics/transfer.sh to include dire/user arguments

--- metagenomics/transfer.sh
+++ metagenomics/transfer.sh
@@ -5,7 +5,11 @@
 ## author: sankaran.kris@gmail.com
 ## date: 11/27/2017
 
+## Specify username
+USR=lanhuong
+## Sepcify data directory to download
+DIR=$SCRATCH/data/
 ## Would be nice if the data are gzipped...
-rsync --compress --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaG/HMD_Mar2017/*Sub/*.fq ../data/metagenomic
-rsync --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Sub/*.fq.gz ../data/metatranscriptomic
-rsync --copy-links --ignore-existing -r kriss1@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Plate/*.fq.gz ../data/metatranscriptomic
+rsync --compress --copy-links --ignore-existing -r $USR@curie.stanford.edu:/relman04/projects/hmd/MetaG/HMD_Mar2017/*Sub/*.fq $DIR/metagenomic
+rsync --copy-links --ignore-existing -r $USR@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Sub/*.fq.gz $DIR/metatranscriptomic
+rsync --copy-links --ignore-existing -r $USR@curie.stanford.edu:/relman04/projects/hmd/MetaT/*Plate/*.fq.gz $DIR/metatranscriptomic
