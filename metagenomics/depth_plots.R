e39ede65763046359b14c436e2323b960dc05663
Kris Sankaran <sankaran.kris@gmail.com>
2017-12-29 22:14:43 -0800

    minor refinements to the metagenomics plots

--- metagenomics/depth_plots.R
+++ metagenomics/depth_plots.R
@@ -44,7 +44,7 @@ theme_update(
 ## Define the parser
 parser <- arg_parser("Plot MIDAS output")
 parser <- add_argument(parser, "--subdir", help = "The subdirectory of data/ containing all the processed data", default = "metagenomic")
-parser <- add_argument(parser, "--k", help = "k in k-over-a filter for depths", default = 0.4)
+parser <- add_argument(parser, "--k", help = "k in k-over-a filter for depths", default = 0.25)
 parser <- add_argument(parser, "--a", help = "a in k-over-a filter for depths", default = 0.0)
 argv <- parse_args(parser)
 
@@ -61,7 +61,7 @@ plot_heatmap <- function(mdepths, fill_type = "Diet_Interval") {
     ) +
     scale_alpha(range = c(0, 1)) +
     scale_fill_interval() +
-    facet_grid(Subject ~ ., scale = "free", space = "free") +
+    facet_grid(Subject ~ species_name, scale = "free", space = "free") +
     theme(
       axis.text = element_blank(),
       strip.text.y = element_text(angle = 0),
@@ -98,6 +98,7 @@ samp <- read_xlsx("../data/Mapping_Files_7bDec2017.xlsx", "Samp", skip = 1) %>%
 ###############################################################################
 ## Prepare data for gene depths plots
 ###############################################################################
+depths <- depths[substr(depths$species, 1, 11) == "Bacteroides", ]
 depths[is.na(depths)] <- 0
 keep_ix <- rowMeans(depths[, -c(1, 2)] > argv$a) > argv$k
 
@@ -108,16 +109,15 @@ depths_df <- depths[keep_ix, ] %>%
     c("genus", "species_name", "strain_id"), "_",
     remove = FALSE
   ) %>%
-  filter(genus == "Bacteroides") %>%
   mutate_at(vars(starts_with("M")), asinh)
 rownames(depths_df) <- depths_df$gene_id
 rm(depths)
 
 ## transformed depths
-depths_df[1:1000, ] %>%
+depths_df[1:10000, ] %>%
   select(starts_with("M")) %>%
   as.matrix() %>%
-  hist(breaks = 20, main = "asinh(Depths)")
+  hist(breaks = 30, main = "asinh(Depths)", ylim = c(0, 2e5))
 
 ## extracting order for plot
 depths_mat <- depths_df %>%
@@ -136,10 +136,11 @@ mdepths <- depths_df %>%
 
 ## data for PCA biplot
 depths_mat <- depths_mat[, apply(depths_mat, 2, var) > 0]
-pc_depths <- princomp(scale(depths_mat))
-scores <- pc_depths$scores %>%
+pc_depths <- princomp(scale(depths_mat, scale = FALSE))
+scores <- pc_depths$scores[, 1:10] %>%
   as.data.frame() %>%
-  rownames_to_column("gene_id")
+  rownames_to_column("gene_id") %>%
+  left_join(depths_df %>% select(gene_id, species))
 
 loadings <- pc_depths$loadings[, 1:5] %>%
   as.data.frame() %>%
@@ -159,15 +160,19 @@ ggplot(scores) +
   geom_hline(yintercept = 0, col = "#e6e6e6") +
   geom_vline(xintercept = 0, col = "#e6e6e6") +
   geom_point(
-    aes(x = Comp.1, y = Comp.2, size = Comp.3),
-    alpha = 0.4
+    aes(x = Comp.1, y = Comp.2, size = Comp.3, col = species),
+    alpha = 0.2
   ) +
   geom_text_repel(
     data = scores %>%
-      filter(Comp.1 ^ 2 + Comp.2 ^ 2 > 1000),
+      filter(Comp.1 ^ 2 + Comp.2 ^ 2 > 3500),
     aes(x = Comp.1, y = Comp.2, label = gene_id),
-    force = 0.002,
+    force = 0.2,
     size = 2.5
+  ) +
+  guides(
+    col = guide_legend(override.aes = list(alpha = 1)),
+    size = guide_legend(override.aes = list(alpha = 1))
   ) +
   scale_size(range = c(0.001, 2))
 
