9a38d92e680dbbbf71312e60e8c9bddd559027b8
Kris Sankaran <sankaran.kris@gmail.com>
2018-02-14 18:37:59 -0800

    remove host sequences

--- metatranscriptomics/workflow_kris.sh
+++ metatranscriptomics/workflow_kris.sh
@@ -65,3 +65,24 @@ $APP_DIR/vsearch-2.7.0-linux-x86_64/bin/vsearch \
     --fastq_filter mouse1_univec_bwa.fastq --fastaout mouse1_univec_bwa.fasta # hack to convert to fasta
 blat -noHead -minIdentity=90 -minScore=65  UniVec_Core mouse1_univec_bwa.fasta -fine -q=rna -t=dna -out=blast8 mouse1_univec.blatout
 $WORK_DIR/1_BLAT_Filter.py mouse1_univec_bwa.fastq mouse1_univec.blatout mouse1_univec_blat.fastq mouse1_univec_blat_contaminats.fastq
+
+## removing host reads (we will want ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/cds/Homo_sapiens.GRCh38.cds.all.fa.gz)
+## exact same logic as removing vectors
+wget ftp://ftp.ensembl.org/pub/current_fasta/mus_musculus/cds/Mus_musculus.GRCm38.cds.all.fa.gz
+gzip -d Mus_musculus.GRCm38.cds.all.fa.gz
+mv Mus_musculus.GRCm38.cds.all.fa mouse_cds.fa
+
+bwa index -a bwtsw mouse_cds.fa
+samtools faidx mouse_cds.fa
+makeblastdb -in mouse_cds.fa -dbtype nucl
+
+bwa mem -t 4 mouse_cds.fa mouse1_univec_blat.fastq > mouse1_mouse_bwa.sam
+samtools view -bS mouse1_mouse_bwa.sam > mouse1_mouse_bwa.bam
+samtools fastq -n -F 4 -0 mouse1_mouse_bwa_contaminats.fastq mouse1_mouse_bwa.bam
+samtools fastq -n -f 4 -0 mouse1_mouse_bwa.fastq mouse1_mouse_bwa.bam
+
+$APP_DIR/vsearch-2.7.0-linux-x86_64/bin/vsearch \
+    --fastq_filter mouse1_mouse_bwa.fastq \
+    --fastaout mouse1_mouse_bwa.fasta
+blat -noHead -minIdentity=90 -minScore=65  mouse_cds.fa mouse1_mouse_bwa.fasta -fine -q=rna -t=dna -out=blast8 mouse1_mouse.blatout
+./1_BLAT_Filter.py mouse1_mouse_bwa.fastq mouse1_mouse.blatout mouse1_mouse_blat.fastq mouse1_mouse_blat_contaminats.fastq
