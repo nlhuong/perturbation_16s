598050c663932d7c517b777152a56ee2177b2b54
Kris Sankaran <sankaran.kris@gmail.com>
2018-02-14 17:28:05 -0800

    create a qc directory, and give better names

--- metatranscriptomics/workflow_kris.sh
+++ metatranscriptomics/workflow_kris.sh
@@ -8,11 +8,12 @@
 ## date: 2/14/2018
 
 ## download and organize the data / scripts
-export WORKDIR = .
-export MT_DIR = ../data/metatranscriptomics/
-export WF_DIR = $MT_DIR/workflow_raw/
+export WORK_DIR=$PWD
+export MT_DIR=../data/metatranscriptomic/
+export WF_DIR=$MT_DIR/workflow_raw/
 rm -rf $WF_DIR
 mkdir $WF_DIR
+mkdir $MT_DIR/QC/
 wget https://github.com/ParkinsonLab/2017-Microbiome-Workshop/releases/download/Extra/precomputed_files.tar.gz
 
 mv precomputed_files.tar.gz $WF_DIR
@@ -20,8 +21,9 @@ cd $WF_DIR
 tar --wildcards -xvf precomputed_files.tar.gz *.py
 tar -xvf precomputed_files.tar.gz mouse1.fastq
 
-cd $WORKDIR
+cd $WORK_DIR
 mv $WF_DIR/*.py .
 
 ## generate the fastqc report
-fastqc $WF_DIR/mouse1.fastqc
+fastqc $WF_DIR/mouse1.fastq
+mv $WF_DIR/*.html $MT_DIR/QC/
