8febbec76b85d6238397e1deccf52110d472a0b4
Kris Sankaran <sankaran.kris@gmail.com>
2018-02-15 13:19:40 -0800

    merge all references genomes into one fasta

--- metatranscriptomics/setup_refs.sh
+++ metatranscriptomics/setup_refs.sh
@@ -6,10 +6,6 @@
 ## author: krissankaran@stanford.edu
 ## date: 2/15/2018
 
-export SCRIPT_DIR=/scratch/users/kriss1/research/perturbation_16s/metatranscriptomics/
-export MT_DIR=$SCRIPT_DIR/../data/metatranscriptomic/
-export REF_DIR=$MT_DIR/references
-export PROCESS_DIR=$MT_DIR/workflow_raw/
 rm -rf $PROCESS_DIR
 mkdir $PROCESS_DIR
 mkdir $REF_DIR
@@ -38,7 +34,12 @@ mv Mus_musculus.GRCm38.cds.all.fa mouse_cds.fa
 wget ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/Rfam.cm.gz
 gunzip Rfam.cm.gz
 
+# Genome and protein annotation dbs
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Bacteria/all.ffn.tar.gz
+mkdir microbial_references
+tar -zxvf all.ffn.tar.gz -C microbial_references/
+find . -name 'microbial_references*.ffn' -exec cat {} \;> microbial_all_cds.fasta
+rm -r microbial_references/
+
 wget ftp://ftp.ncbi.nih.gov/blast/db/FASTA/nr.gz
-tar -zxvf all.ff.nn.tar.gz
 gunzip nr.gz
