e22cd0e233c0c8b24e7f6c063ffefc0b81413e19
Kris Sankaran <sankaran.kris@gmail.com>
2018-03-15 16:49:34 -0700

    some more annotation stats

--- metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
+++ metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
@@ -53,21 +53,24 @@ def read_filter_stats(output_dir, subject_id, sample_id):
         read_filter_stats(output_dir, subject_id, sample_id)
     """
     stats = OrderedDict()
+    subject_dir = os.path.join(output_dir, subject_id)
+
+    def make_path(dir, suffix):
+        return os.path.join(subject_dir, dir, sample_id + suffix)
 
     ## statistics about trimming / quality filtering
-    subject_dir = os.path.join(output_dir, subject_id)
-    trim_file = os.path.join(subject_dir, "trimmed", sample_id + "_trim.fq")
-    qual_file = os.path.join(subject_dir, "main", sample_id + "_qual.fq")
+    trim_file = make_path("trimmed", "_trim.fq")
+    qual_file = make_path("main", "_qual.fq")
     stats["n_trim"] = file_len(trim_file) / 4.0
     stats["n_qual"] = file_len(qual_file) / 4.0
     stats["trim_to_qual"] = 1 - stats["n_qual"] / stats["n_trim"]
 
     ## statistics about vector and host read removal
-    unique = os.path.join(subject_dir, "main", sample_id + "_unique.fq")
-    vector_bwa = os.path.join(subject_dir, "aligned", sample_id + "_univec_bwa.fq")
-    vector_blat = os.path.join(subject_dir, "aligned", sample_id + "_univec_blat.fq")
-    host_bwa = os.path.join(subject_dir, "aligned", sample_id + "_human_bwa.fq")
-    host_blat = os.path.join(subject_dir, "aligned", sample_id + "_human_blat.fq")
+    unique = make_path("main", "_unique.fq")
+    vector_bwa = _make_path("aligned", "_univec_bwa.fq")
+    vector_blat = make_path("aligned", "_univec_blat.fq")
+    host_bwa = make_path("aligned", "_human_bwa.fq")
+    host_blat = make_path("aligned", "_human_blat.fq")
 
     stats["unique"] = file_len(unique) / 4.0
     stats["vector_bwa"] = file_len(vector_bwa) / 4.0
@@ -78,7 +81,7 @@ def read_filter_stats(output_dir, subject_id, sample_id):
     stats["vector_to_human"] = 1 - stats["human_blat"] / stats["vector_blat"]
 
     ## statistics about rRNA filtering
-    mRNA = os.path.join(subject_dir, "main", sample_id + "_unique_mRNA.fq")
+    mRNA = make_path("main", "_unique_mRNA.fq")
     stats["mRNA"] = file_len(mRNA) / 4.0
     stats["rRNA_removal"] = 1 - stats["mRNA"] / stats["human_blat"]
     return stats
@@ -88,15 +91,31 @@ def annotation_stats(output_dir, subject_id, sample_id):
      Assess the quality of read annotation
     """
     stats = OrderedDict()
-
     subject_dir = os.path.join(output_dir, subject_id)
-    genus_file = os.path.join(subject_dir, "taxonomy", sample_id + "_genus_class_summary.txt")
+
+    def make_path(dir, suffix):
+        return os.path.join(subject_dir, dir, suffix)
+
+    genus_file = os.path.join(subject_dir, "taxonomy", "_genus_class_summary.txt")
     genus_summary = split("\n|\t", tail(genus_file, 3))
 
     stats["genus_unassigned_perc"] = float(genus_summary[0])
     stats["genus_unassigned_reads"] = float(genus_summary[1])
     stats["genus_unclassified_perc"] = float(genus_summary[4])
     stats["genus_unclassified_reads"] = float(genus_summary[5])
+
+    contigs = make_path("assembled", "_contigs.fasta")
+    unassembled = make_path("assembled", "unassembled.fq")
+    contigs_unmapped = make_path("assembled", "contigs_unmapped.fq")
+    unassembled_unmapped = make_path("assembled", "unassembled_unmapped.fq")
+
+    stats["contigs"] = file_len(contigs) / 4.0
+    stats["unassembled"] = file_len(unassembled) / 4.0
+    stats["contigs_unmapped"] = file_len(contigs_unmapped) / 4.0
+    stats["contigs_unmapped_perc"] = stats["contigs_unmapped"] / stats["contigs"]
+    stats["unassembled_unmapped"] = file_len(unassembled_unmapped) / 4.0
+    stats["unassembled_unmapped_perc"] = stats["unassembled_unmapped"] / stats["unassembled"]
+
     return stats
 
 
