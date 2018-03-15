c5fefc10406adcc8e3a5442490a1f5199f233918
Kris Sankaran <sankaran.kris@gmail.com>
2018-03-15 16:40:32 -0700

    annotation and assembly statistics

--- metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
+++ metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
@@ -15,6 +15,7 @@ date: 03/15/2018
 import subprocess
 import os
 from collections import OrderedDict
+from re import split
 
 def file_len(fname):
     """
@@ -26,6 +27,19 @@ def file_len(fname):
     if p.returncode != 0:
         raise IOError(err)
     return int(result.strip().split()[0])
+
+
+def tail(f, n):
+    """
+    https://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail
+    """
+    p = subprocess.Popen(
+        ["tail", "-n", str(n), f],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    result, err = p.communicate()
+    return result
 
 
 def read_filter_stats(output_dir, subject_id, sample_id):
@@ -62,5 +76,43 @@ def read_filter_stats(output_dir, subject_id, sample_id):
     stats["human_bwa"] = file_len(vector_bwa) / 4.0
     stats["human_blat"] = file_len(vector_bwa) / 4.0
     stats["vector_to_human"] = 1 - stats["human_blat"] / stats["vector_blat"]
+
+    ## statistics about rRNA filtering
+    mRNA = os.path.join(subject_dir, "main", sample_id + "_unique_mRNA.fq")
+    stats["mRNA"] = file_len(mRNA) / 4.0
+    stats["rRNA_removal"] = 1 - stats["mRNA"] / stats["human_blat"]
+    return stats
+
+def annotation_stats(output_dir, subject_id, sample_id):
+    """
+     Assess the quality of read annotation
+    """
+    stats = OrderedDict()
+
+    subject_dir = os.path.join(output_dir, subject_id)
+    genus_file = os.path.join(subject_dir, "taxonomy", sample_id + "_genus_class_summary.txt")
+    genus_summary = split("\n|\t", tail(genus_file, 3))
+
+    stats["genus_unassigned_perc"] = float(genus_summary[0])
+    stats["genus_unassigned_reads"] = float(genus_summary[1])
+    stats["genus_unclassified_perc"] = float(genus_summary[4])
+    stats["genus_unclassified_reads"] = float(genus_summary[5])
+    return stats
+
+
+def assembly_stats(output_dir, subject_id, sample_id):
+    """
+    Assess the quality of the assembly
+
+    """
+    stats = OrderedDict()
+
+    subject_dir = os.path.join(output_dir, subject_id)
+    original = os.path.join(subject_dir, "assembled", sample_id + "_mRNA.fq")
+    unassembled = os.path.join(subject_dir, "assembled", sample_id + "_unassembled.fq")
+    unassembled = os.path.join(subject_dir, "assembled", sample_id + "_contigs.fq")
+    stats["original"] = file_len(original) / 4.0
+    stats["unassembled"] = file_len(unassembled) / 4.0
+    stats["unassembled_prop"] = stats["unassembled"] / stats["original"]
 
     return stats
