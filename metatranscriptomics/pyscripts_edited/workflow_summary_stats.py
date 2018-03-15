ef5f24bac246ea24230bdd8b71aeb575f18deebf
Kris Sankaran <sankaran.kris@gmail.com>
2018-03-15 15:37:21 -0700

    order the entries, and convert to floats

--- metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
+++ metatranscriptomics/pyscripts_edited/workflow_summary_stats.py
@@ -14,6 +14,7 @@ date: 03/15/2018
 
 import subprocess
 import os
+from collections import OrderedDict
 
 def file_len(fname):
     """
@@ -37,29 +38,29 @@ def read_filter_stats(output_dir, subject_id, sample_id):
         sample_id = "M3303_DBUsw_2r_TrM31"
         read_filter_stats(output_dir, subject_id, sample_id)
     """
-    stats = dict()
+    stats = OrderedDict()
 
     ## statistics about trimming / quality filtering
     subject_dir = os.path.join(output_dir, subject_id)
     trim_file = os.path.join(subject_dir, "trimmed", sample_id + "_trim.fq")
     qual_file = os.path.join(subject_dir, "main", sample_id + "_qual.fq")
-    stats["n_trim"] = file_len(trim_file) / 4
-    stats["n_qual"] = file_len(qual_file) / 4
-    stats["trim_to_qual"] = 1 - float(stats["n_qual"]) / stats["n_trim"]
+    stats["n_trim"] = file_len(trim_file) / 4.0
+    stats["n_qual"] = file_len(qual_file) / 4.0
+    stats["trim_to_qual"] = 1 - stats["n_qual"] / stats["n_trim"]
 
     ## statistics about vector and host read removal
     unique = os.path.join(subject_dir, "main", sample_id + "_unique.fq")
     vector_bwa = os.path.join(subject_dir, "aligned", sample_id + "_univec_bwa.fq")
-    vector_blat = os.path.join(subject_dir, "aligned", sample_id + "_univec_blat_contam.fq")
+    vector_blat = os.path.join(subject_dir, "aligned", sample_id + "_univec_blat.fq")
     host_bwa = os.path.join(subject_dir, "aligned", sample_id + "_human_bwa.fq")
     host_blat = os.path.join(subject_dir, "aligned", sample_id + "_human_blat.fq")
 
-    stats["unique"] = file_len(unique) / 4
-    stats["vector_bwa"] = file_len(vector_bwa) / 4
-    stats["vector_blat"] = file_len(vector_blat) / 4
+    stats["unique"] = file_len(unique) / 4.0
+    stats["vector_bwa"] = file_len(vector_bwa) / 4.0
+    stats["vector_blat"] = file_len(vector_blat) / 4.0
     stats["unique_to_vector"] = 1 - stats["vector_blat"] / stats["unique"]
-    stats["human_bwa"] = file_len(vector_bwa) / 4
-    stats["human_blat"] = file_len(vector_bwa) / 4
+    stats["human_bwa"] = file_len(vector_bwa) / 4.0
+    stats["human_blat"] = file_len(vector_bwa) / 4.0
     stats["vector_to_human"] = 1 - stats["human_blat"] / stats["vector_blat"]
 
     return stats
