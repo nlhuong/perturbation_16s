8a4a93f255ba357f6a6a477e3eb149f3fe812b0e
Lan Huong Nguyen <nlhuong90@gmail.com>
2018-03-27 23:14:38 -0700

    fix wrapper files

--- metatranscriptomics/wrapper_submit.sh
+++ metatranscriptomics/wrapper_submit.sh
@@ -12,25 +12,24 @@ PI_BASE_DIR=$PI_SCRATCH/resilience/metatranscriptomics
 declare -a arr=("Relman_RNAseq_16" "Relman_RNAseq_17/" "Relman_RNAseq_18")
 for SUBDIR in "${arr[@]}"
 do
+    IN=${1:-$PI_BASE_DIR/raw/$SUBDIR}
+    OUT=${2:-$PI_BASE_DIR/processed/$SUBDIR}
+    LOG=${3:-$PI_BASE_DIR/logs/$SUBDIR}
 
-IN=${1:-$PI_BASE_DIR/raw/$SUBDIR}
-OUT=${2:-$PI_BASE_DIR/processed/$SUBDIR}
-LOG=${3:-$PI_BASE_DIR/logs/$SUBDIR}
+    TIME=${3:-24:00:00}
+    NCPUS=${4:-8}
+    MEM=${5:-6G}
 
-TIME=${3:-24:00:00}
-NCPUS=${4:-8}
-MEM=${5:-6G}
-
-cd $IN
-mkdir -p $OUT
-for fwd_file in *_R1_001.fastq; do
-    base="$(echo $fwd_file | cut -d '_' -f1-3)"
-    if [ ! -f $OUT/counts/dmnd_SEED/${base}* ]; then
-        echo $base
-        FWD=$fwd_file
-        REV=${FWD%_R1_001.fastq}_R2_001.fastq
-        bash $CODE_DIR/submit.sh $IN $OUT $FWD $REV $LOG $TIME $NCPUS $MEM
-        sleep 1 
-    fi
+    cd $IN
+    mkdir -p $OUT
+    for fwd_file in *_R1_001.fastq; do
+        base="$(echo $fwd_file | cut -d '_' -f1-3)"
+        if [ ! -f $OUT/counts/dmnd_SEED/${base}* ]; then
+            echo $base
+            FWD=$fwd_file
+            REV=${FWD%_R1_001.fastq}_R2_001.fastq
+            bash $CODE_DIR/submit.sh $IN $OUT $FWD $REV $LOG $TIME $NCPUS $MEM
+            sleep 1 
+        fi
+    done
 done
-
