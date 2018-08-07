d3f9437af3730db95b62e74e92150f891e786e71
Lan Huong Nguyen <nlhuong90@gmail.com>
2018-08-06 23:40:08 -0700

    update metat workflow files

--- metatranscriptomics/wrapper_submit.sh
+++ metatranscriptomics/wrapper_submit.sh
@@ -13,21 +13,33 @@ PI_BASE_DIR=$PI_SCRATCH/resilience/metatranscriptomics
 #for SUBDIR in "${arr[@]}"
 #do
 
-SUBDIR=Arati_R_plate_9
+SUBDIR=Arati_R_plate_11
 #SUBDIR=Relman_RNAseq_18
 
 IN=${1:-$PI_BASE_DIR/raw/$SUBDIR}
 OUT=${2:-$PI_BASE_DIR/processed/$SUBDIR}
-LOG=${3:-$PI_BASE_DIR/logs/$SUBDIR/sh_uniref_2}
+LOG=${3:-$PI_BASE_DIR/logs/$SUBDIR/sh_uniref/sh_count}
 
 TIME=${3:-24:00:00}
 NCPUS=${4:-8}
 MEM=${5:-8G}
 
+
 cd $IN
 mkdir -p $OUT
+running_jobs=($(squeue -u $USER | awk '{ gsub("wf-","",$3); print $3 }'))
 for fwd_file in *_R1_001.fastq; do
     base="$(echo $fwd_file | cut -d '_' -f1-3)"
+    base_running=false
+    for job in "${running_jobs[@]}"; do
+        if [[ "$base" == *${job}* ]]; then
+            base_running=true
+        fi
+    done
+    if [[ $base_running == true ]]; then
+        echo "sample ${base} already running." 
+        continue
+    fi
     if [ ! -s $OUT/counts/dmnd_UniRef/${base}_uniref_abund.csv ]; then
         echo $base
         FWD=$fwd_file
--- metatranscriptomics/wrapper_submit_diamond.sh
+++ metatranscriptomics/wrapper_submit_diamond.sh
@@ -13,23 +13,33 @@ PI_BASE_DIR=$PI_SCRATCH/resilience/metatranscriptomics
 #for SUBDIR in "${arr[@]}"
 #do
 
-#SUBDIR=Arati_R_plate_9
-SUBDIR=Relman_RNAseq_16
+#SUBDIR=Arati_R_plate_11
+SUBDIR=Relman_RNAseq_17
 
 IN=${1:-$PI_BASE_DIR/raw/$SUBDIR}
 OUT=${2:-$PI_BASE_DIR/processed/$SUBDIR}
 LOG=${3:-$PI_BASE_DIR/logs/$SUBDIR/sh_uniref}
 
 TIME=${3:-24:00:00}
-NCPUS=${4:-12}
+NCPUS=${4:-16}
 MEM=${5:-8G}
 
 cd $IN
 mkdir -p $OUT
+running_jobs=($(squeue -u $USER | awk '{ gsub("wf-","",$3); print $3 }'))
 for fwd_file in *_R1_001.fastq; do
     base="$(echo $fwd_file | cut -d '_' -f1-3)"
+    base_running=false
+    for job in "${running_jobs[@]}"; do
+        if [[ "$base" == *${job}* ]]; then
+            base_running=true
+        fi
+    done
+    if [[ $base_running == true ]]; then
+        echo "sample ${base} already running."
+        continue
+    fi
     if [ ! -s $OUT/diamond/${base}_uniref.dmdout ]; then
-    #if [ ! -s $OUT/counts/dmnd_UniRef/${base}_uniref_abund.csv ]; then
         echo $base
         FWD=$fwd_file
         REV=${FWD%_R1_001.fastq}_R2_001.fastq
